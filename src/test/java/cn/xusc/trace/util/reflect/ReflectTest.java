1a0f470a829b38a2ab5a11d0ba46de7d6bc6fd43
Cw404 <18573707104@163.com>
2022-09-21 16:07:41 +0800

    feat:Add ReflectTest

---
+++ src/test/java/cn/xusc/trace/util/reflect/ReflectTest.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2022 WangCai.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.xusc.trace.util.reflect;
+
+import cn.xusc.trace.exception.TraceException;
+import cn.xusc.trace.util.Jsons;
+import cn.xusc.trace.util.reflect.model.ReflectModel;
+import cn.xusc.trace.util.reflect.model.ReflectShowModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 反射测试
+ *
+ * @author wangcai
+ */
+public class ReflectTest {
+
+    @Test
+    @DisplayName("show reflect info of ReflectModel")
+    public void showReflectInfoTest() {
+        new ReflectorShow(new ReflectModel()).classInfo().constructorInfo().fieldInfo().methodInfo().printInfo();
+    }
+
+    /**
+     * 反射显示
+     * @param <T> 反射对象类型
+     */
+    class ReflectorShow<T> {
+
+        /**
+         * 对象类
+         */
+        private Class<T> clazz;
+        /**
+         * 反射显示模型
+         */
+        private ReflectShowModel reflectShowModel;
+
+        /**
+         * 默认构造
+         *
+         * @param t 反射对象
+         */
+        public ReflectorShow(T t) {
+            clazz = new Class<>(t);
+            reflectShowModel = new ReflectShowModel();
+        }
+
+        /**
+         * 类信息
+         *
+         * @return 反射显示自身
+         */
+        public ReflectorShow classInfo() {
+            reflectShowModel.name = clazz.name();
+            reflectShowModel.type = clazz.type();
+            reflectShowModel.componentType = clazz.componentType();
+            reflectShowModel.annotationShowModels = annotationShowModels(clazz.annotations());
+            return this;
+        }
+
+        /**
+         * 构造函数信息
+         *
+         * @return 反射显示自身
+         */
+        public ReflectorShow constructorInfo() {
+            reflectShowModel.constructorShowModels =
+                clazz
+                    .constructors()
+                    .stream()
+                    .map(constructor ->
+                        new ReflectShowModel.ConstructorShowModel(
+                            constructor.name(),
+                            constructor
+                                .parameters()
+                                .stream()
+                                .map(parameter ->
+                                    new ReflectShowModel.ParameterShowModel(
+                                        parameter.name(),
+                                        parameter.type(),
+                                        annotationShowModels(parameter.annotations())
+                                    )
+                                )
+                                .collect(Collectors.toList()),
+                            annotationShowModels(constructor.annotations())
+                        )
+                    )
+                    .collect(Collectors.toList());
+            return this;
+        }
+
+        /**
+         * 字段信息
+         *
+         * @return 反射显示自身
+         */
+        public ReflectorShow fieldInfo() {
+            reflectShowModel.filedShowModels =
+                clazz
+                    .fields()
+                    .stream()
+                    .map(field ->
+                        new ReflectShowModel.FiledShowModel(
+                            field.name(),
+                            field.type(),
+                            field.value(),
+                            annotationShowModels(field.annotations())
+                        )
+                    )
+                    .collect(Collectors.toList());
+            return this;
+        }
+
+        /**
+         * 方法信息
+         *
+         * @return 反射显示自身
+         */
+        public ReflectorShow methodInfo() {
+            reflectShowModel.methodShowModels =
+                clazz
+                    .methods()
+                    .stream()
+                    .map(method ->
+                        new ReflectShowModel.MethodShowModel(
+                            method.name(),
+                            method.returnType(),
+                            method
+                                .parameters()
+                                .stream()
+                                .map(parameter ->
+                                    new ReflectShowModel.ParameterShowModel(
+                                        parameter.name(),
+                                        parameter.type(),
+                                        annotationShowModels(parameter.annotations())
+                                    )
+                                )
+                                .collect(Collectors.toList()),
+                            annotationShowModels(method.annotations())
+                        )
+                    )
+                    .collect(Collectors.toList());
+            return this;
+        }
+
+        /**
+         * 注释显示模型
+         *
+         * @param annotations 注释列表
+         * @return 注释显示模型列表
+         */
+        private List<ReflectShowModel.AnnotationShowModel> annotationShowModels(
+            List<Annotation<Class<? extends java.lang.annotation.Annotation>>> annotations
+        ) {
+            return annotations
+                .stream()
+                .map(annotation ->
+                    new ReflectShowModel.AnnotationShowModel(
+                        annotation.name(),
+                        annotation.type(),
+                        annotation.componentType(),
+                        annotation.value()
+                    )
+                )
+                .collect(Collectors.toList());
+        }
+
+        /**
+         * 打印信息
+         */
+        public void printInfo() {
+            Jsons.write(objectMapper -> {
+                try {
+                    System.out.println(objectMapper.writeValueAsString(reflectShowModel));
+                } catch (JsonProcessingException e) {
+                    throw new TraceException(e);
+                }
+            });
+        }
+    }
+}
---
+++ src/test/java/cn/xusc/trace/util/reflect/model/ReflectShowModel.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2022 WangCai.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.xusc.trace.util.reflect.model;
+
+import java.util.List;
+
+/**
+ * 反射显示模型
+ *
+ * @author wangcai
+ */
+public class ReflectShowModel {
+
+    /**
+     * 名字
+     */
+    public String name;
+    /**
+     * 类型
+     */
+    public Object type;
+    /**
+     * 组件类型
+     */
+    public Object componentType;
+    /**
+     * 构造函数显示模型列表
+     */
+    public List<ConstructorShowModel> constructorShowModels;
+    /**
+     * 注释显示模型列表
+     */
+    public List<AnnotationShowModel> annotationShowModels;
+    /**
+     * 字段显示模型列表
+     */
+    public List<FiledShowModel> filedShowModels;
+    /**
+     * 方法显示模型列表
+     */
+    public List<MethodShowModel> methodShowModels;
+
+    /**
+     * 构造函数显示模型
+     */
+    public static class ConstructorShowModel {
+
+        /**
+         * 名字
+         */
+        public String name;
+        /**
+         * 参数显示模型列表
+         */
+        public List<ParameterShowModel> parameterShowModels;
+        /**
+         * 注释显示模型列表
+         */
+        public List<AnnotationShowModel> annotationShowModels;
+
+        /**
+         * 基础构造
+         *
+         * @param name 名字
+         * @param parameterShowModels 参数显示模型列表
+         * @param annotationShowModels 注释显示模型列表
+         */
+        public ConstructorShowModel(
+            String name,
+            List<ParameterShowModel> parameterShowModels,
+            List<AnnotationShowModel> annotationShowModels
+        ) {
+            this.name = name;
+            this.parameterShowModels = parameterShowModels;
+            this.annotationShowModels = annotationShowModels;
+        }
+    }
+
+    /**
+     * 字段显示模型
+     */
+    public static class FiledShowModel {
+
+        /**
+         * 名字
+         */
+        public String name;
+        /**
+         * 类型
+         */
+        public Object type;
+        /**
+         * 值
+         */
+        public Object value;
+        /**
+         * 注释显示模型列表
+         */
+        public List<AnnotationShowModel> annotationShowModels;
+
+        /**
+         * 基础构造
+         *
+         * @param name 名字
+         * @param type 类型
+         * @param value 值
+         * @param annotationShowModels 注释显示模型列表
+         */
+        public FiledShowModel(String name, Object type, Object value, List<AnnotationShowModel> annotationShowModels) {
+            this.name = name;
+            this.type = type;
+            this.value = value;
+            this.annotationShowModels = annotationShowModels;
+        }
+    }
+
+    /**
+     * 方法显示模型
+     */
+    public static class MethodShowModel {
+
+        /**
+         * 名字
+         */
+        public String name;
+        /**
+         * 返回类型
+         */
+        public String returnType;
+        /**
+         * 参数显示模型列表
+         */
+        public List<ParameterShowModel> parameterShowModels;
+        /**
+         * 注释显示模型列表
+         */
+        public List<AnnotationShowModel> annotationShowModels;
+
+        /**
+         * 基础构造
+         * @param name 名字
+         * @param returnType 返回类型
+         * @param parameterShowModels 参数显示模型列表
+         * @param annotationShowModels 注释显示模型列表
+         */
+        public MethodShowModel(
+            String name,
+            String returnType,
+            List<ParameterShowModel> parameterShowModels,
+            List<AnnotationShowModel> annotationShowModels
+        ) {
+            this.name = name;
+            this.returnType = returnType;
+            this.parameterShowModels = parameterShowModels;
+            this.annotationShowModels = annotationShowModels;
+        }
+    }
+
+    /**
+     * 参数显示模型
+     */
+    public static class ParameterShowModel {
+
+        /**
+         * 名字
+         */
+        public String name;
+        /**
+         * 类型
+         */
+        public Object type;
+        /**
+         * 注释显示模型列表
+         */
+        public List<AnnotationShowModel> annotationShowModels;
+
+        /**
+         * 基础构造
+         *
+         * @param name 名字
+         * @param type 类型
+         * @param annotationShowModels 注释显示模型列表
+         */
+        public ParameterShowModel(String name, Object type, List<AnnotationShowModel> annotationShowModels) {
+            this.name = name;
+            this.type = type;
+            this.annotationShowModels = annotationShowModels;
+        }
+    }
+
+    /**
+     * 注释显示模型
+     */
+    public static class AnnotationShowModel {
+
+        /**
+         * 名字
+         */
+        public String name;
+        /**
+         * 类型
+         */
+        public Object type;
+        /**
+         * 组件类型
+         */
+        public Object componentType;
+        /**
+         * 值
+         */
+        public Object value;
+
+        /**
+         * 基础构造
+         *
+         * @param name 名字
+         * @param type 类型
+         * @param componentType 组件类型
+         * @param value 值
+         */
+        public AnnotationShowModel(String name, Object type, Object componentType, Object value) {
+            this.name = name;
+            this.type = type;
+            this.componentType = componentType;
+            this.value = value;
+        }
+    }
+}
